d3c6f9f110e691450f3047f44a317ec8511a27a8
Adam Prescott <adam@aprescott.com>
2015-07-21 18:16:32 -0400

    Limit push sizes and spread jobs over multiple pushes.
    `push_bulk` will now split pushes into groups. Those groups will, by
    default, be at most 10,000 jobs in size.
    
    For example, `push_bulk([1, 2, 3, 4, 5], limit: 3)` will limit each
    push to at most 3 items. That makes
    
       push_bulk([1, 2, 3, 4, 5], limit: 3) { ... }
    
    equivalent to pushing `[1, 2, 3]`, then `[4, 5]` separately. The default
    `limit:` value is `10_000`.
    
    `push_bulk!` is also added as a way to push in bulk without any
    spreading -- all jobs are pushed in one go.

--- lib/sidekiq/bulk.rb
+++ lib/sidekiq/bulk.rb
@@ -1,5 +1,13 @@
+require "active_support/core_ext/array/grouping"
+
 module SidekiqBulk
-  def push_bulk(items, &block)
+  def push_bulk(items, limit: 10_000, &block)
+    items.in_groups_of(limit, false).each do |group|
+      push_bulk!(group, &block)
+    end
+  end
+
+  def push_bulk!(items, &block)
     if block
       args = items.map(&block)
     else
