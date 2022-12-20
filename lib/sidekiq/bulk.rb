e99b7d1a7c39b7e14b3384b2afd134159bcb759c
Md. Farhan Memon <simpl-farhan@Mds-MacBook-Pro.local>
2022-12-20 15:43:10 +0530

    support for bulk push at specific time in future added

--- lib/sidekiq/bulk.rb
+++ lib/sidekiq/bulk.rb
@@ -1,6 +1,28 @@
-require "sidekiq"
+require 'sidekiq'
 
 module SidekiqBulk
+  def push_bulk_in(interval, items, limit: 10_000, &block)
+    int = interval.to_f
+    now = Time.now.to_f
+    timestamp = (int < 1_000_000_000 ? now + int : int)
+
+    job_ids = items.each_slice(limit).map do |group|
+      push_bulk_in!(timestamp, group, &block)
+    end
+
+    job_ids.flatten
+  end
+
+  def push_bulk_in!(timestamp, items, &block)
+    args = if block
+             items.map(&block)
+           else
+             items.map { |el| [el] }
+           end
+
+    Sidekiq::Client.push_bulk('class' => self, 'args' => args, 'at' => timestamp)
+  end
+
   def push_bulk(items, limit: 10_000, &block)
     job_ids = items.each_slice(limit).map do |group|
       push_bulk!(group, &block)
@@ -10,13 +32,13 @@ module SidekiqBulk
   end
 
   def push_bulk!(items, &block)
-    if block
-      args = items.map(&block)
-    else
-      args = items.map { |el| [el] }
-    end
+    args = if block
+             items.map(&block)
+           else
+             items.map { |el| [el] }
+           end
 
-    Sidekiq::Client.push_bulk("class" => self, "args" => args)
+    Sidekiq::Client.push_bulk('class' => self, 'args' => args)
   end
 end
 
